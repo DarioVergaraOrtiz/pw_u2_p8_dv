b14ad13d30a3065b962bf83af090eb697fbad85f
DarioVergaraOrtiz <167145189+DarioVergaraOrtiz@users.noreply.github.com>
2025-05-12 23:17:55 +0000

    Configuración del vue.config.js por defecto

--- arranque-vue/vue.config.js
+++ arranque-vue/vue.config.js
@@ -1,25 +1,4 @@
-/*
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-})
-*/
-
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: 'all',
-    client: {
-      webSocketURL: {
-        protocol: 'wss',
-        hostname: process.env.CODESPACE_NAME + '-8080.githubpreview.dev',
-        port: 443,
-        pathname: '/ws',
-      }
-    }
-  }
 })
