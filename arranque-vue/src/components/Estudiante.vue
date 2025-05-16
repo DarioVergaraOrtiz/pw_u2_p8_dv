23b0b25a019c332f4a0104e17eeb4c97404d7b43
DarioVergaraOrtiz <167145189+DarioVergaraOrtiz@users.noreply.github.com>
2025-05-16 14:13:34 -0500

    Tarea3

--- arranque-vue/src/components/Estudiante.vue
+++ arranque-vue/src/components/Estudiante.vue
@@ -4,11 +4,17 @@
     <input v-model="nuevoNombre" id="id_nombre" type="text" />
     <label for="id_apellido">Apellido:</label>
     <input v-model="nuevoApellido" id="id_apellido" type="text" />
+    <label for="id_edad">Edad:</label>
+    <input v-model="nuevoEdad" id="id_edad" type="Number" />
+    <label for="id_genero">Genero:</label>
+    <input v-model="nuevoGenero" id="id_genero" type="text" />
+    <label for="id_apodo">Apodo:</label>
+    <input v-model="nuevoApodo" id="id_apodo" type="text" />
     <button @click="agregarEstudiante()">Agregar Estudiante</button>
 
     <ul>
-      <li v-for="{ nombre, apellido } in lista" :key="nombre">
-        Nombre: {{ nombre }} - Apellido: {{ apellido }}
+      <li v-for="{ nombre, apellido, edad, genero, apodo } in lista" :key="nombre">
+        Nombre: {{ nombre }} - Apellido: {{ apellido }} - Edad: {{ edad }} - Genero: {{ genero }} - Apodo: {{ apodo }}
       </li>
     </ul>
   </div>
@@ -20,14 +26,17 @@ export default {
     return {
       nuevoNombre: null,
       nuevoApellido: null,
+      nuevoEdad: null,
+      nuevoGenero: null,
+      nuevoApodo: null,
       lista: [
-        { nombre: "Dario", apellido: "Vergara" },
-        { nombre: "Belen", apellido: "Paca" },
-        { nombre: "Andy", apellido: "Cueva" },
-        { nombre: "Bob", apellido: "Esponja" },
-        { nombre: "Patricio", apellido: "Estrella" },
-        { nombre: "Cala", apellido: "Mardo" },
-        { nombre: "Calixto", apellido: "Ortiz" },
+        { nombre: "Dario", apellido: "Vergara", edad:24, genero:"Masculino", apodo:"Calixto" },
+        { nombre: "Belen", apellido: "Paca", edad:25, genero:"Femenino", apodo:"Bel" },
+        { nombre: "Andy", apellido: "Cueva", edad:25, genero:"Masculino", apodo:"Jagandy" },
+        { nombre: "Bob", apellido: "Esponja", edad:26, genero:"Masculino", apodo:"Calixto2" },
+        { nombre: "Patricio", apellido: "Estrella", edad:27, genero:"Masculino", apodo:"Calixto3" },
+        { nombre: "Cala", apellido: "Mardo", edad:28, genero:"Masculino", apodo:"Calixto4" },
+        { nombre: "Calixto", apellido: "Ortiz", edad:29, genero:"Masculino", apodo:"Calixto5" },
       ],
     };
   },
@@ -36,6 +45,9 @@ export default {
       const nuevo = {
         nombre: this.nuevoNombre,
         apellido: this.nuevoApellido,
+        edad: this.nuevoEdad,
+        genero: this.nuevoGenero,
+        apodo: this.nuevoApodo
       };
       this.lista.unshift(nuevo);
       /* this.lista.push(nuevo);*/
